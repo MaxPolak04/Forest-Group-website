8375c07cd87520c31b2d27b65f3abd60e34eb173
MaxPolak04 <140717832+MaxPolak04@users.noreply.github.com>
2024-02-27 15:58:13 +0100

    I started styling the nav

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const footerYear = document.querySelector('.footer__year')
+
+
+
+
+
+
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear()
+    footerYear.innerText = year
+}
+
+handleCurrentYear()
